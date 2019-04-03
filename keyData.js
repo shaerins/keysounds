a9ec7628ca714211fd50c71c151ee25e222b2242
shaerins <srcbun@gmail.com>
2019-04-03 14:37:15 -0400

    add shrinking animations, start key colors

---
+++ keyData.js
@@ -0,0 +1,20 @@
+var keys = {
+  q: {
+    color: "purple",
+    sound: new Howl({
+      src: ["sounds/bubbles.mp3"]
+    })
+  },
+  w: {
+    color: "red",
+    sound: new Howl({
+      src: ["sounds/clay.mp3"]
+    })
+  },
+  e: {
+    color: "blue",
+    sound: new Howl({
+      src: ["sounds/confetti.mp3"]
+    })
+  }
+}
